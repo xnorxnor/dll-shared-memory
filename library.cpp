cd39774a1fffa610ed60e0035103404c8e539321
xnorxnor <xnor-github@xnor.in>
2019-05-16 20:56:05 +0200

    cleanup

--- library.cpp
+++ library.cpp
@@ -4,15 +4,11 @@
 
 void hello()
 {
-  //AllocConsole();
-  //freopen("CONOUT$", "w", stdout);
   std::cout << "Hello from DLL" << "\n";
 }
 
-//BOOL APIENTRY DllMain ( HMODULE hModule, DWORD reason, LPVOID lpReserved ) {
 extern "C" BOOL APIENTRY DllMain(HMODULE /*module*/, DWORD reason, LPVOID /*reserved*/)
 {
-//bool __stdcall DllMain(HMODULE /*module*/, DWORD reason, LPVOID /*reserved*/) {
   if (reason == DLL_PROCESS_ATTACH)
   {
     //MessageBoxA(NULL,"test","test",NULL);
