6b711e74c95afe088464ce1f4cb1e4ec72ccdadb
xnorxnor <xnor-github@xnor.in>
2019-05-27 10:38:52 +0200

    function prototypes and basic types

--- src/shared_memory.h
+++ src/shared_memory.h
@@ -7,8 +7,30 @@
 
 #include <string>
 #include <vector>
+#include <map>
+
+// simple struct with "processed" data, produced by the DLL
+struct ProcessedData
+{
+  std::string result;
+  std::string unit;
+  std::vector<std::string> listOfLotNumbers;
+  std::string dateTimeStamp;
+};
+
+enum class LogLevel
+{
+  warning,
+  info,
+  error
+};
 
 typedef bool (*CallBackFunctionFromHostToDllPtr)();
+
+typedef std::string (*RequestUserInputFromHost) ();
+typedef bool (*RequestDataFromDatabaseWithTimeout) (const std::string &query, std::map<std::string, std::vector<std::string>> &dataBaseTable, unsigned int maximumSecondsToWait);
+typedef void (*LogDataFromDll)(LogLevel logLevel, std::string& logEntry);
+typedef void (*SendProcessedDataToHost) (const std::vector<ProcessedData>& listOfProcessedDataItems);
 
 struct SharedMemory
 {
@@ -16,5 +38,9 @@ struct SharedMemory
   std::string inputFromHost;
   std::vector<std::string> stringsSharedByDllAndHost;
   CallBackFunctionFromHostToDllPtr callBackFunctionFromHostToDll {nullptr};
+  RequestUserInputFromHost  requestUserInputFromHost {nullptr};
+  RequestDataFromDatabaseWithTimeout requestDataFromDatabaseWithTimeout {nullptr};
+  LogDataFromDll logDataFromDll {nullptr};
+  SendProcessedDataToHost sendProcessedDataToHost {nullptr};
 };
 #endif //DLL_TEST_SHARED_MEMORY_H
